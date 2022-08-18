b2bff2925c940ef1983383bf1d78a225b73404ea
Serkan Aydın <aydinserkan2004@gmail.com>
2022-08-18 12:03:43 +0300

    18.08.2022 3. ders ödevi

---
+++ ClassMetotDemo/MusteriManageri.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManageri
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler, Müşteri eklendi: " + musteri.Adi + " " + musteri.Soyadi);
+        }
+
+        public void Silme(int Id)
+        {
+            Console.WriteLine(Id + " numaralı müşteri Başarıyla Silindi");
+        }
+
+        public void Listeleme(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi + " " + musteri.Soyadi);
+            }
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,26 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Serkan";
+            musteri1.Soyadi = "Aydın";
+            musteri1.Yas = 38;
+            musteri1.TelNo = "505050505";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Sümeyye";
+            musteri2.Soyadi = "Aydın";
+            musteri2.Yas = 37;
+            musteri2.TelNo = "505050505";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManageri musteriManageri = new MusteriManageri();
+            musteriManageri.Ekle(musteri1);
+
+            musteriManageri.Silme(musteri1.Id);
+
+            musteriManageri.Listeleme(musteriler);
         }
     }
 }
